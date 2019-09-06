b8b966399ffa1db0fe5b421c7f172521514ab12b
Javier Rodriguez Saiz <rodriguezsaizjavier@gmail.com>
2019-09-06 11:26:00 +0200

    antes de las tablas

--- chollometro/Home.py
+++ chollometro/Home.py
@@ -1,20 +1,86 @@
+from pandas import DataFrame
 
 from chollometro.drivers.ChromeDriver import *
+from chollometro.model.Product import Product
+import pandas as pd
 
 driver().get("https://www.chollometro.com/nuevos")
 
-HOMELIST = driver().find_elements_by_css_selector(".cept-listing--card > div")
-NAMES = driver().find_elements_by_css_selector("strong.thread-title")
-PRICES = driver().find_elements_by_css_selector("span.thread-price")
+COLUMNS = ["CATEGORY", "IMAGE", "NAME", "PRICE", "OLD_PRICE", "RATE", "DELIVERY", "MSG", "USER", "LINK"]
 
-titles = [name.text for name in NAMES]
-prices = [price.text for price in PRICES]
-catalog = []
+CATALOG = driver().find_elements_by_css_selector("section.gridLayout > div.gridLayout-item > article")
 
-listamapeada = list(map(lambda x, y: x+ ' -> ' +y, titles, prices))
+catalog = [item for item in CATALOG]
 
-for x in listamapeada:
-    print(x)
+products_catalog = []
 
-# time.sleep(3)
+for item in catalog:
+
+    try:
+        category = item.find_element_by_css_selector(
+            "section.gridLayout > div.gridLayout-item > article > div > div > span").text
+    except:
+        category = "..."
+
+    try:
+        image = item.find_element_by_class_name("thread-image").get_attribute("src")
+    except:
+        image = "..."
+    try:
+        name = item.find_element_by_class_name("thread-title").text
+    except:
+        name = "..."
+    try:
+        price = item.find_element_by_class_name("thread-price").text
+    except:
+        price = "..."
+    try:
+        old_price = item.find_element_by_css_selector(
+            "section.gridLayout > div.gridLayout-item > article > div:nth-child(4) > span:nth-child(2) > span:nth-child(1)").text
+    except:
+        old_price = "..."
+    try:
+        rate = item.find_element_by_css_selector(
+            "section.gridLayout > div.gridLayout-item > article > div:nth-child(4) > span:nth-child(2) > span:nth-child(2)").text
+    except:
+        rate = "..."
+    try:
+        delivery = item.find_element_by_css_selector(
+            "section.gridLayout > div.gridLayout-item > article > div:nth-child(4) > span:nth-child(3) > span > span").text
+    except:
+        delivery = "..."
+    try:
+        msg = item.find_element_by_css_selector(
+            "section.gridLayout > div.gridLayout-item > article > div:nth-child(5) > div > div").text
+    except:
+        msg = "..."
+    try:
+        user = item.find_element_by_class_name("thread-username").text
+    except:
+        user = "..."
+    try:
+        link = item.find_element_by_css_selector(
+            "section.gridLayout > div.gridLayout-item > article > div:nth-child(7) > a").get_attribute("href")
+    except:
+        link = "..."
+
+    products_catalog.append(Product(category,
+                                    image,
+                                    name,
+                                    price,
+                                    old_price,
+                                    rate,
+                                    delivery,
+                                    msg,
+                                    user,
+                                    link
+                                    ))
+df: DataFrame = DataFrame()
+
+
+for x in products_catalog:
+    # print(x.get_name() + " -> " + x.get_price() + " -> " + x.get_rate())
+
+    df.append(pd.DataFrame(products_catalog,COLUMNS))
+print(df)
 driver().close()
--- chollometro/model/Product.py
+++ chollometro/model/Product.py
@@ -1,17 +1,59 @@
 class Product:
 
-    def __init__(self, name:str, price:int):
+    def __init__(self, category, image, name, price, old_price, rate, delivery, msg, user, link):
+        self.__category = category
+        self.__image = image
         self.__name = name
         self.__price = price
+        self.__old_price = old_price
+        self.__rate = rate
+        self.__delivery = delivery
+        self.__msg = msg
+        self.__user = user
+        self.__link = link
 
-    def get_name(self):
-        return self.__name
+    def get_category(self): return self.__category
 
-    def get_price(self):
-        return self.__price
+    def set_category(self, category): self.__category = category
 
-    def set_name(self, name):
-        self.__name = name
+    def get_image(self): return self.__image
 
-    def set_price(self, price):
-        self.__price = price
+    def set_image(self, image): self.__image = image
+
+    def get_name(self): return self.__name
+
+    def set_name(self, name): self.__name = name
+
+    def get_price(self): return self.__price
+
+    def set_price(self, price): self.__price = price
+
+    def get_old_price(self): return self.__old_price
+
+    def set_old_price(self, old_price): self.__old_price = old_price
+
+    def get_rate(self): return self.__rate
+
+    def set_rate(self, rate): self.__rate = rate
+
+    def get_delivery(self): return self.__delivery
+
+    def set_delivery(self, delivery): self.__delivery = delivery
+
+    def get_msg(self): return self.__msg
+
+    def set_msg(self, msg): self.__msg = msg
+
+    def get_user(self): return self.__user
+
+    def set_user(self, user): self.__user = user
+
+    def get_link(self): return self.__link
+
+    def set_link(self, link): self.__link = link
+
+
+
+
+
+
